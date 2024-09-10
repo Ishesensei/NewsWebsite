51ead44bda1ede22303fc9a5942ac17500193168
Amazing Sensei <ishesensei@gmail.com>
2024-09-10 16:59:12 +0530

    interception and url navigated image is working, due to using default.js instead of page.js in slot @folder

--- app/news/[slug]/test/page.js
+++ app/news/[slug]/test/page.js
@@ -1,5 +1,7 @@
-export default function TestRoutepage() {
+import React from 'react'
+
+export default function page() {
   return (
-    <div>test route visible!!</div>
+    <div>page visible!!</div>
   )
 }
