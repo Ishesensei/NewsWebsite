9a5d1c77fba3d5d02498d88b18ef1209b65e6a97
Amazing Sensei <ishesensei@gmail.com>
2024-09-11 22:26:35 +0530

    implemented fetching news using next feature a react server component RSC with async can be used to fetch directly in the page.

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,38 +1,16 @@
-'use client';
-import { useEffect, useState } from 'react';
-
 import NewsList from '@/components/news-list';
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState();
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
-  useEffect(() => {
-    setIsLoading(true);
-    const fetchNews = async () => {
-      const response = await fetch('http://localhost:8080/news');
-      if (!response.ok) {
-        setError('Fail to fetch news!');
-        setIsLoading(false);
-      }
-
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-      console.log('✌️news --->', news);
-    };
-    fetchNews();
-  }, []);
-  let newsContent;
-  if (news) {
-    newsContent = <NewsList news={news} />;
+export default async function NewsPage() {
+  const response = await fetch('http://localhost:8080/news');
+  if (!response.ok) {
+    throw new Error('Fail to fetch news!');
   }
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      {isLoading && !error && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {!isLoading && !error && newsContent}
+      <NewsList news={news} />
     </>
   );
 }
