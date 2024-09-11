665edfd09051b715d88d0e1bd39dcf63f52b1d5f
Amazing Sensei <ishesensei@gmail.com>
2024-09-11 17:48:33 +0530

    Fetch news from backend using react hooks - usestate and useeffect, in news page only

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,38 @@
+'use client';
+import { useEffect, useState } from 'react';
+
 import NewsList from '@/components/news-list';
-import { DUMMY_NEWS } from '@/dummy-news';
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchNews = async () => {
+      const response = await fetch('http://localhost:8080/news');
+      if (!response.ok) {
+        setError('Fail to fetch news!');
+        setIsLoading(false);
+      }
+
+      const news = await response.json();
+      setIsLoading(false);
+      setNews(news);
+      console.log('✌️news --->', news);
+    };
+    fetchNews();
+  }, []);
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {isLoading && !error && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && newsContent}
     </>
   );
 }
